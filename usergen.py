fe5a49795f98ceed35bd3af8e3830465aba293e3
coltn <tools.thrutch@aleeas.com>
2021-09-03 00:36:27 -0500

    initial commit

---
+++ usergen.py
@@ -0,0 +1,32 @@
+# Username Generator
+
+import random
+import math
+from pairs_output import pairs
+
+amount = int(input("Number of Usernames? "))
+length = int(input("Length of Usernames? "))
+
+print("\n")
+
+# divides length by 2 since each pair already has 2 letters
+k_val = length / 2
+
+# if the length was an odd number, this rounds up the decimal from a float to an int
+if isinstance(k_val, float):
+    k_val = int(math.ceil(k_val))
+
+def UserGen():
+    name = "".join(random.choices(pairs, k=k_val))
+    # cuts off the last letter if length is odd and too long
+    if len(name) > length:
+        name = name[:length]
+    print(name)
+    with open('usernames.txt', 'a') as t:
+        t.write(name + '\n')
+    return name
+
+for i in range(amount):
+    UserGen()
+
+print("\n\nDone. Appended to usernames.txt")
